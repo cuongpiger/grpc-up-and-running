498121890128105cc0fa90bcdfbc8d4cd55b87cb
Cuong. Duong Manh <cuongpigerr@gmail.com>
2025-06-21 13:42:34 +0700

    chore: nothing to commit

--- chap02/server/main.go
+++ chap02/server/main.go
@@ -53,8 +53,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
 	s := grpc.NewServer()
 	pb.RegisterProductInfoServer(s, &server{})
+
+	log.Println("gRPC server is running on port " + port)
+
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
