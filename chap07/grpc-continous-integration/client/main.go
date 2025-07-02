3f67b3c07e65a55744b25dfa38befd7c017cd602
Cuong. Duong Manh <cuongpigerr@gmail.com>
2025-07-02 21:33:56 +0700

    chore: nothing to commit

---
+++ chap07/grpc-continous-integration/client/main.go
@@ -0,0 +1,49 @@
+// Go to ${grpc-up-and-running}/samples/ch02/productinfo
+// Optional: Execute protoc -I proto-gen proto-gen/product_info.proto-gen --go_out=plugins=grpc:go/product_info
+// Execute go get -v github.com/grpc-up-and-running/samples/ch02/productinfo/golang/product_info
+// Execute go run go/client/main.go
+
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/cuongpiger/golang/ecommerce"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewProductInfoClient(conn)
+
+	// Contact the server and print out its response.
+	name := "Sumsung S10"
+	description := "Samsung Galaxy S10 is the latest smart phone, launched in February 2019"
+	price := float32(700.0)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.AddProduct(ctx, &pb.Product{Name: name, Description: description, Price: price})
+	if err != nil {
+		log.Fatalf("Could not add product: %v", err)
+	}
+	log.Printf("Product ID: %s added successfully", r.Value)
+
+	product, err := c.GetProduct(ctx, &pb.ProductID{Value: r.Value})
+	if err != nil {
+		log.Fatalf("Could not get product: %v", err)
+	}
+	log.Printf("Product: ", product.String())
+}
